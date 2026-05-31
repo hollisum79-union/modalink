95d66af9163d492e13e79366f22323df601a5414
hollisum79-union <hollisum79@gmail.com>
2026-05-31 17:24:38 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1355,13 +1355,12 @@ function CanteenScreen({ onBack, user }) {
             gap: 8,
           }}
         >
-          <Icon
+                    <Icon onClick={() => allDates.length > 0 && setShowDates(!showDates)}
             path="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
             color="#fff"
             size={16}
           />
-          <span style={{ fontSize: 14, color: "#fff", fontWeight: 600 }}>
-            {dateStr}
+          <span onClick={() => allDates.length > 0 && setShowDates(!showDates)} style={{ fontSize: 14, color: "#fff", fontWeight: 600, cursor: "pointer" }}>
             {viewDate === todayKey ? dateStr : ("2026년 " + viewDate.replace("/", "월 ") + "일")}
           </span>
         </div>
