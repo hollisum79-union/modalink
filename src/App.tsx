a731b2284ae6ecfaaee136935422fd2c0a45b67c
hollisum79-union <hollisum79@gmail.com>
2026-06-08 16:10:59 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14360,12 +14360,12 @@ const getKyobunWork = (member: any, date: Date) => {
         margin: "0 auto",
         fontFamily: "'Apple SD Gothic Neo', 'Noto Sans KR', sans-serif",
                         background: "#F4F3FF",
-                        height: "100dvh",
+                                height: "100dvh",
         display: "flex",
         flexDirection: "column",
         overflow: "hidden",
         boxSizing: "border-box",
-        paddingBottom: 80,
+        paddingBottom: 64,
       }}
     >
       {/* 헤더 */}
