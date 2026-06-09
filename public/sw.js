e3639ba37c15d06fb6237d09f481fc4994d03f28
hollisum79-union <hollisum79@gmail.com>
2026-06-09 10:04:07 +0900

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -11,7 +11,7 @@ self.addEventListener("push", (event) => {
     icon: "/icon-192.png",
     badge: "/icon-192.png",
     data: { url: data.url || "/" },
-    vibrate: [100, 50, 100],
+    vibrate: [300, 150, 300, 150, 300],
   };
   event.waitUntil(self.registration.showNotification(title, options));
 });
